9bfcd5fa7cc32bdbb12b6bbb65574cbf9a26239d
Mark Corbett <37854406+speleonut@users.noreply.github.com>
2023-10-10 14:21:01 +1030

    add bulk gzip

---
+++ utilities/bulkGzip.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#SBATCH -J zipItGood
+#SBATCH -o /hpcfs/users/%u/log/gzip.slurm-%j.out
+
+#SBATCH -A robinson
+#SBATCH -p batch                        # partition (this is the queue your job will be added to)
+#SBATCH -N 1                            # number of nodes (due to the nature of sequential processing, here uses single node)
+#SBATCH -n 1                            # number of cores (here uses 8)
+#SBATCH --time=08:00:00                 # time allocation, which has the format (D-HH:MM)
+#SBATCH --mem=4G                       # memory pool for all cores (here set to 4 GB)
+
+# Notification configuration
+#SBATCH --mail-type=END                 # Type of email notifications will be sent (here set to END, which means an email will be sent when the job is done)
+#SBATCH --mail-type=FAIL                # Type of email notifications will be sent (here set to FAIL, which means an email will be sent when the job is fail to complete)
+#SBATCH --mail-user=%u@adelaide.edu.au  # Email to which notification will be sent
+
+#Script Paths
+userDir=/hpcfs/users/${USER}
+
+# bulkGzip.sh
+usage()
+{
+echo "# bulkGzip.sh save space by compressing all the things.
+#
+# Usage: 
+# single sample: sbatch $0 -i /path/to/input-file | [ -h | --help ]
+# multiple samples: sbatch --array <0-(n-1) samples> $0 -i /path/to/input-file | [ -h | --help ]
+#
+# Options:
+# -i <arg>           REQUIRED: List of files with full or relative (used cautiously) file paths.
+# -h | --help        Prints the message you are reading.
+#
+# History:
+# Script created by: Mark Corbett on 01/07/2022
+# email:mark dot corbett is at adelaide university
+# Modified (Date; Name; Description):
+# 10/10/2023; Mark; Add to map-n-call and update packages for new HPC
+#
+"
+}
+
+# Parse script options
+while [ "$1" != "" ]; do
+    case $1 in
+        -i )    shift
+                inputFile=$1
+                ;;
+        -h | --help )   usage
+                        exit 0
+                        ;;
+        * )     usage
+                exit 1
+    esac
+    shift
+done
+
+# Check that your script has everything it needs to start.
+if [ ! -z "$inputFile" ]; then
+    usage
+    echo "#ERROR: You need to specify -i /path/to/input-file
+    exit 1
+fi
+
+module load gzip/1.10-GCCcore-11.2.0
+readarray -t fileToCompress < $inputFile
+
+# When a big file comes along, you must zip it
+gzip ${fileToCompress[$SLURM_ARRAY_TASK_ID]}
