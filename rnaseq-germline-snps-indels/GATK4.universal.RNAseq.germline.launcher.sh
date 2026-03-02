558e62cd385a9d9957a7273576c1ffec6195c48b
Mark Corbett <37854406+speleonut@users.noreply.github.com>
2026-03-03 09:54:17 +1030

    start RNAseq germline calling pipeline

---
+++ rnaseq-germline-snps-indels/GATK4.universal.RNAseq.germline.launcher.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# A script to launch GATK4 Universal RNAseq Germline SNPs and Indels pipeline on Phoenix
+## Hard coded paths for your system should be set in configs/BWA-GATKHC.environment.cfg ##
+whereAmI="$(dirname "$(readlink -f "$0")")" # Assumes that the script is linked to the git repo and the driectory structure is not broken
+configDir="$(echo ${whereAmI} | sed -e 's,rnaseq-germline-snps-indels,configs,g')"
+enviroCfg="${configDir}/BWA-GATKHC.environment.cfg"
+source ${enviroCfg}
+
+if [ ! -d "${logDir}" ]; then
+    mkdir -p ${logDir}
+    echo "## INFO: New log directory created, you'll find all of the log information from this pipeline here: ${logDir}"
+fi
+
+usage() {
+echo "# This script coordinates the launch of the GATK4 Universal RNAseq Germline SNPs and Indels pipeline on Phoenix. It requires an output directory and a config file as input. The config file should contain all necessary parameters for the pipeline, including paths to reference genomes, input data, and any specific settings for the GATK tools.
+# Requirements: RNAseq fastq files or mapped (bam / cram) files.
+#
+# Usage: $0 -i /path/to/input.file.txt -o /path/to/output/directory/ -c /path/to/config/file.cfg | [-h | --help]
+#
+# Options:
+# -i             REQUIRED: Path and file name of a text file with sequences listed in the form \"read-group-ID path/to/read_1-1,...,path/to/read_n-1 /path/to/read_1-2,...,/path/to/read_n-2\"
+#                          OR path to a text file with a list of paths to bam or cram files. Note this is slower to run.
+#                          Don't mix the two formats in the same file. If you have both, split them into two files and run the pipeline separately for each file.
+# -o             OPTIONAL: Output directory where the results will be stored. This directory will be created if it does not exist.
+# -c             OPTIONAL: Path to the configuration file that contains all necessary parameters for the pipeline.
+# -h | --help    OPTIONAL: Display this help message and exit.
+#
+# History: 
+# Script created by: Mark Corbett on 02/03/2026
+# email: mark dot corbett is at adelaide.edu.au
+# Modified (Date; Name; Description):
+#
+"
+}
+
+# Parse script options
+while [ "$1" != "" ]; do
+	case $1 in
+        -i )    shift
+                seqFile=$1
+			    ;;
+        -o )    shift
+                outDir=$1
+                ;;
+        -c )    shift
+                config=$1
+                ;;
+        -h | --help )    usage
+                         exit 0
+                         ;;
+        * )    usage
+               exit 1
+    esac
+    shift
+done
+
+# Check the input file
+if [ -z "${seqFile}" ]; then
+    echo "## ERROR: No input file provided. Please provide a text file with the required format using the -i option."
+    usage
+    exit 1
+fi  
+
+# Check the input file format and trust the user to have put the correct information in there (for now).
+if [ $(awk -F"\t" 'NR==1{print NF}' ${seqFile}) -ne 3 ]; then
+    if [ -f $(head -n 1 ${seqFile}) ]; then
+        bamInput=true
+        echo "## INFO: ${seqFile} has $(awk -F"\t" 'NR==1{print NF}' ${seqFile}) column. If that number is not 1 then there are some problems with the input file and bad things are going to happen."
+        fi
+else
+    bamInput=false
+fi
+
+# Check the output directory and if it wasn't provided create a default directory.
+if [ -z "${outDir}" ]; then
+    outDir="${useDir}/variants/GATK4_universal_RNAseq_germline_snps_indels"
+    echo "## INFO: No output directory provided. A default directory will be created at ${outDir}."
+fi
+
+if [ ! -d "${outDir}" ]; then
+    mkdir -p ${outDir}
+    echo "## INFO: New output directory created, you'll find all of the results from this pipeline here: ${outDir}"
+fi
+
+# Check the config and if not specified use the default.
+if [ -z "${config}" ]; then
+    config="${configDir}/GATK4.RNAseq.germline.hg38.phoenix.cfg"
+    echo "## INFO: No config file provided. A default config file will be used at ${config}."
+fi
